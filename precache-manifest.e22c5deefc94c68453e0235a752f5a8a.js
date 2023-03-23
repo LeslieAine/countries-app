031dfb3d6ee118fbce7b61d29fcdf5cb7bca387f
LeslieAine <aineleslie@gmail.com>
2023-03-23 17:43:32 +0300

    Updates

---
+++ precache-manifest.e22c5deefc94c68453e0235a752f5a8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a9a81329a74a2d61dc7",
+    "url": "/countries-app/static/css/main.449c492f.chunk.css"
+  },
+  {
+    "revision": "6a9a81329a74a2d61dc7",
+    "url": "/countries-app/static/js/main.9797a0ac.chunk.js"
+  },
+  {
+    "revision": "3c5e6ac5695d49f7116b",
+    "url": "/countries-app/static/js/runtime~main.59456884.js"
+  },
+  {
+    "revision": "e0cca55d4f37841952dd",
+    "url": "/countries-app/static/js/2.599f915d.chunk.js"
+  },
+  {
+    "revision": "357bf1e25b6f5e678032528eaa89e9cd",
+    "url": "/countries-app/index.html"
+  }
+];
